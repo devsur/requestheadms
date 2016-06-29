15d9e6d0511ad3e20fd39946574ca12c2575a506
x <x>
2016-06-29 13:43:36 -0300

    add final

---
+++ final/rhpms.js
@@ -0,0 +1,16 @@
+/* ** request header microservice ** - devsur */
+var accepts = require('accepts');
+
+exports.getHead = function(request) {
+	var ip = request.connection.remoteAddress; //or express req.ip
+	var language = accepts(request).languages()[0];
+	var os = request.headers["user-agent"];
+
+	var headers = {
+		ip: ip,
+		language: language,
+		os: os
+	};
+	
+	return headers;
+};
